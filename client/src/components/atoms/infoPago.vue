2043508117467ae444264d104d76c6c617071343
Axel Anaya <axel_anaya@hotmail.com>
2020-10-01 21:49:22 -0500

    Cambios del front

---
+++ client/src/components/atoms/infoPago.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="info-pago">
+    <p>{{ concepto }}: </p>
+    <p>{{ infoConcepto }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'infoPago',
+  props: {
+    concepto: {
+      type: String,
+      required: true
+    },
+    infoConcepto: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+.info-pago{
+    display: flex;
+    flex-direction: row;
+    font-weight: bold;
+    margin-bottom: 10px;
+}
+.info-pago > :first-child {
+    color: rgb(10, 82, 189);
+    margin-right: 5px;
+}
+</style>
---
+++ client/src/components/pages/Checkout.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <div class="inner-container">
+      <div>
+        <infoPago v-for="(val, key) in informacionPago" :key="key" :concepto="key" :infoConcepto="val"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import infoPago from '../atoms/infoPago'
+
+export default {
+  name: 'prueba',
+  data() {
+    return {
+      informacionPago: {
+        'Concepto de pago':'REALIZACION DE EVENTO DE SUBASTA', 
+        'Tipo de bien':'CASA'
+      }
+    }
+  },
+  components: {
+    infoPago
+  }
+}
+</script>
+
+<style scoped>
+.container{
+  display: flex;
+  align-items: stretch;
+  height: 100vh;
+  width: 100%;
+}
+.inner-container{
+  -webkit-box-shadow: 0px 0px 14px -6px #000000; 
+  box-shadow: 0px 0px 14px -6px #000000;
+  background: rgb(243, 243, 243);
+  flex-grow: 1;
+  margin: 100px 200px;
+}
+</style>
