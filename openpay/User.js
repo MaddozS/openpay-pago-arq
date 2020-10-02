4a370f12a59824df3c6e7f0c7838b5541efc218a
Axel Anaya <axel_anaya@hotmail.com>
2020-10-01 20:32:59 -0500

    getId function now is async

--- openpay/User.js
+++ openpay/User.js
@@ -8,21 +8,23 @@ const createUser = ( { name, last_name, email, phone_number, address } ) => ({
         phone_number,
         address
     },
-    async getId() {
-        OPENPAY.customers.create(this.fields, (error, body) => {
-            if(error){
-                console.log({'status':'error', 'message': error})
-                Promise.reject({'status':'error', 'message': error})
-            }
-            else{
-                console.log({'status':'OK', 'message': body})
-                Promise.resolve({'status':'OK', 'message': body})
-            }
-            //error;    // null if no error occurred (status code != 200||201||204)
-            //body;     // contains the object returned if no error occurred (status code == 200||201||204)
-        });
+    getId: async function() {
+
+        return new Promise((resolve, reject) =>{
+            OPENPAY.customers.create(this.fields, (error, body) => {
+                if(error){
+                    console.log({'status':'error', 'message': error})
+                    reject({'status':'error', 'message': error})
+                }
+                else{
+                    console.log({'status':'OK', 'message': body})
+                    resolve({'status':'OK', 'message': body})
+                }
+                //error;    // null if no error occurred (status code != 200||201||204)
+                //body;     // contains the object returned if no error occurred (status code == 200||201||204)
+            });
+        })
     }
 })
-
 
 module.exports = { createUser }
