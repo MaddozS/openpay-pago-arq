7ae0589bf173fd1295c95252319f299cd408c7d7
Axel Anaya <axel_anaya@hotmail.com>
2020-10-02 11:40:43 -0500

    pruebas de client añadidas

--- client/src/components/pages/Checkout.vue
+++ client/src/components/pages/Checkout.vue
@@ -4,11 +4,17 @@
       <div>
         <infoPago v-for="(val, key) in informacionPago" :key="key" :concepto="key" :infoConcepto="val"/>
       </div>
+      <form action="" method="get" id='formCard'>
+
+      </form>
+      <p>{{ id }}</p>
+      <p>{{ response }}</p>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import infoPago from '../atoms/infoPago'
 
 export default {
@@ -18,8 +24,33 @@ export default {
       informacionPago: {
         'Concepto de pago':'REALIZACION DE EVENTO DE SUBASTA', 
         'Tipo de bien':'CASA'
-      }
+      },
+      id: 'NO ID',
+      response: ''
     }
+  },
+  mounted(){
+    // const plugin = document.createElement("script");
+    // plugin.setAttribute(
+    //   "src",
+    //   "https://resources.openpay.mx/lib/openpay-data-js/1.2.38/openpay-data.v1.min.js"
+    // );
+    let deviceSessionId = window.OpenPay.deviceData.setup("formCard", "deviceIdHiddenFieldName");
+    //console.log(deviceSessionId)
+    this.id = deviceSessionId;
+
+
+    axios.post('api/payment/create_card/', {
+      device_session_id: this.id
+    })
+    .then(res => {
+      this.response = res.data
+    })
+    .catch(error => {
+      console.log('aaa')
+      console.log(error)
+      this.response = error.data
+    })
   },
   components: {
     infoPago
